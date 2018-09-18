77a0e8b5245a22e49961b90c6965aecf2dce163f
abacsrob <abacsrob@gmail.com>
2018-09-18 17:34:31 +0200

    first

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,18 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JblogappJBlogModule } from './j-blog/j-blog.module';
+import { JblogappBlogModule } from './blog/blog.module';
+import { JblogappEntryModule } from './entry/entry.module';
+import { JblogappTagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        JblogappJBlogModule,
+        JblogappBlogModule,
+        JblogappEntryModule,
+        JblogappTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { LoginModalService, Principal, Account } from 'app/core';
+import {JBlogService} from 'app/entities/j-blog';
+import {IJBlog, JBlog} from 'app/shared/model/j-blog.model';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 
 @Component({
     selector: 'jhi-home',
@@ -12,14 +15,21 @@ import { LoginModalService, Principal, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    blogs: JBlog[];
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager, private jblogService: JBlogService) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        this.jblogService.query().subscribe(
+            (res: HttpResponse<IJBlog[]>) => {
+                this.blogs = res.body;
+            },
+            (res: HttpErrorResponse) => { console.log(res); }
+        );
     }
 
     registerAuthenticationSuccess() {
